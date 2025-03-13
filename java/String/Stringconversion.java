4cedb9361e314e3b802a54b2c3eefd7d3aaded34
shrutipanchal02 <shruti.panchal@vmvinfotech.com>
2025-03-13 18:15:54 +0530

    Add files via upload

---
+++ java/String/Stringconversion.java
@@ -0,0 +1,10 @@
+class Stringconversion {  
+    public static void main(String[] args) {  
+        String str = "shruti";  
+        int a=10;
+        String s1=String.valueOf(a);//any value which is any datatype value convert into string here
+        System.out.println(a); 
+          System.out.println(str.toUpperCase()); //convert into upper case
+            System.out.println(str.toLowerCase()); //convert into lower case
+        }
+        }

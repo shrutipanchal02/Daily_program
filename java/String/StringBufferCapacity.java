c68f94600350b223f20c4e3b29ab8df3fb5c5012
shrutipanchal02 <shruti.panchal@vmvinfotech.com>
2025-03-13 17:48:26 +0530

    Add files via upload

---
+++ java/String/StringBufferCapacity.java
@@ -0,0 +1,6 @@
+class StringBufferCapacity{
+public static void main(String[] args){
+StringBuffer sb=new StringBuffer("shruti");
+System.out.println(sb.capacity());// 16 + string length
+}
+}
---
+++ java/String/stringBufferLength.java
@@ -0,0 +1,6 @@
+class stringBufferLength{
+public static void main(String[] args){
+StringBuffer sb=new StringBuffer("shruti");
+System.out.println(sb.length());
+
+}

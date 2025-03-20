27ad81504f11c60f587456590657a6e5e8957dd6
shrutipanchal02 <shruti.panchal@vmvinfotech.com>
2025-03-20 22:38:48 +0530

    Add files via upload

---
+++ Collection/arraylistsize.java
@@ -0,0 +1,18 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class arraylistsize {
+	public static void main(String[] args) {
+		   ArrayList al = new ArrayList();
+		        al.add(100);
+		        al.add("shruti");
+		        al.add('c');
+		        
+		        System.out.println(al);     
+
+		        System.out.println(al.size());
+}
+}
+
+

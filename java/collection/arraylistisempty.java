eec456f8882f48ab7a7630d2aff2fa3518701301
shrutipanchal02 <shruti.panchal@vmvinfotech.com>
2025-03-20 22:41:48 +0530

    Add files via upload

---
+++ java/collection/arraylistisempty.java
@@ -0,0 +1,18 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class arraylistisempty {
+	public static void main(String[] args) {
+		   ArrayList al = new ArrayList();
+		        al.add(100);
+		        al.add("shruti");
+		        al.add('c');
+		        
+		        System.out.println(al);     
+
+		        System.out.println(al.isEmpty());
+}
+}
+
+

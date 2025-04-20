71d05bc7ae38f4b9fcf470b1aa172852612828f1
shrutipanchal02 <shruti.panchal@vmvinfotech.com>
2025-04-20 15:50:58 +0530

    Add files via upload

---
+++ java/StarPattern/starPattern15.java
@@ -0,0 +1,17 @@
+class starPattern15{
+public static void main(String[] args){
+for(int i=1;i<=4;i++)
+{
+   for(int j=4;i>=i;j--)
+   {
+     System.out.print(" ");
+   }
+   for(k=1;k<=i;k++)
+   {
+    System.out.print(" * ");
+   }
+ }
+    System.out.print();
+    }
+    }
+     

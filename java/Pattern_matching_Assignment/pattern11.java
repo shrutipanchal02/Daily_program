9483ac9495aab0af65a8048e331086d3a0b41e8f
shrutipanchal02 <shruti.panchal@vmvinfotech.com>
2026-03-10 16:15:51 +0530

    Add files via upload

---
+++ java/Pattern_matching_Assignment/pattern11.java
@@ -0,0 +1,19 @@
+class pattern11{
+public static void main(String[] args)
+{
+for (int i=1;i<=4;i++)
+{
+for(int j=1;j<=4;j++)
+{
+System.out.print(" a ");
+}
+System.out.println();
+}
+}
+}
+
+/*output: 
+ a  a  a  a 
+ a  a  a  a 
+ a  a  a  a 
+ a  a  a  a */
